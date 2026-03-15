1c3fdf1e368f348bb93ae46a7224bc40c6c9d806
dxtym <dilmurod.abdusamadov2004@gmail.com>
2026-03-15 12:12:40 +0500

    fix

--- app/next.config.ts
+++ app/next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  serverExternalPackages: ["postgres"],
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "lh3.googleusercontent.com" },

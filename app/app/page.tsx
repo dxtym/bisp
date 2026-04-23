37d19da2a767646377e0f9baff536d6af09e3ece
dxtym <dilmurod.abdusamadov2004@gmail.com>
2026-04-23 16:12:34 +0500

    refactor(layout): remove inline panel from sidebar

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -5,14 +5,12 @@ import { Loader } from "@/components/ai-elements/loader";
 import Main from "@/components/main";
 import Chat from "@/components/chat";
 import Menu from "@/components/menu";
-import Panel from "@/components/panel";
 import { Provider } from "@/components/providers/store";
 import {
   SidebarProvider,
   SidebarInset,
   SidebarTrigger,
 } from "@/components/ui/sidebar";
-import { Separator } from "@/components/ui/separator";
 
 export default function Home() {
   const { data: session, status } = useSession();
@@ -35,17 +33,13 @@ export default function Home() {
         <Menu />
         <SidebarInset className="flex flex-col h-[calc(100svh-1rem)] overflow-hidden">
           <div className="flex flex-1 h-full">
-            <div className="flex-2 overflow-y-auto">
+            <div className="flex-1 overflow-y-auto">
               <div className="p-3 border-b">
                 <SidebarTrigger />
               </div>
               <div className="h-[calc(100vh-1rem-3.5rem)]">
                 <Chat />
               </div>
-            </div>
-            <Separator orientation="vertical" />
-            <div className="flex-1 overflow-y-auto">
-              <Panel />
             </div>
           </div>
         </SidebarInset>
